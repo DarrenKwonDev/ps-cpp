f9fec9143f02f87db3607a13a76ffafc40cd9557
darrenkwondev <darrenkwondev46@gmail.com>
2023-10-26 17:28:07 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a;
+    cin >> a;
+
+    if (a % 400 == 0 || (a % 4 == 0 && a % 100 != 0)) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+
+    return 0;
+}
