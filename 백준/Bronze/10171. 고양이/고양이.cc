9a231163d4c674f5bca750319e05c23eae84769e
darrenkwondev <darrenkwondev46@gmail.com>
2023-10-26 17:05:11 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string a = R"(\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+    )";
+
+    cout << a;
+
+    return 0;
+}
