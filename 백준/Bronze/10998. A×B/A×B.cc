6cf3e2ea13da6f6e26f535f7ef3d2d27c1ffccdc
darrenkwondev <darrenkwondev46@gmail.com>
2023-10-28 02:54:25 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <assert.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a * b;
+}
