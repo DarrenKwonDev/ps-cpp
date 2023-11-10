ae53298c5e31cfa7ed3271c4210c662c911e4a47
darrenkwondev <darrenkwondev46@gmail.com>
2023-11-11 07:38:01 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    std::cout << "|\\_/|\n";
+    std::cout << "|q p|   /}\n";
+    std::cout << "( 0 )\"\"\"\\\n";
+    std::cout << "|\"^\"`    |\n";
+    std::cout << "||_/=\\\\__|\n";
+
+    return 0;
+}
