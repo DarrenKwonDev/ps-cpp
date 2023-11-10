a4fa6c4a096afdea0acbe0e97a9dc73a7880eca5
darrenkwondev <darrenkwondev46@gmail.com>
2023-11-11 07:53:26 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.precision(10);
+
+    double a, b;
+    cin >> a >> b;
+
+    cout << a / b;
+
+    return 0;
+}
