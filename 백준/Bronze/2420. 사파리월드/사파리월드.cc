edeeabd9e7337f22158aaf2f97541c7c9a10f4ad
darrenkwondev <darrenkwondev46@gmail.com>
2023-11-11 07:58:29 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long a, b;
+    cin >> a >> b;
+    if (a > b) {
+        cout << a - b;
+    } else {
+        cout << b - a;
+    }
+
+    return 0;
+}
