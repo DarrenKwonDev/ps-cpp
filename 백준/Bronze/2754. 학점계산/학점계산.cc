fde491806738f1b82b5f8fec432e060e9b783320
darrenkwondev <darrenkwondev46@gmail.com>
2023-11-11 11:39:45 +0900

    [Bronze V] Title: 학점계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string s;
+    cin >> s;
+    cout.precision(1);
+
+    switch (s[0]) {
+    case 'A':
+        if (s[1] == '+') {
+            cout << "4.3";
+        } else if (s[1] == '0')
+            cout << "4.0";
+        else {
+            cout << "3.7";
+        }
+
+        break;
+    case 'B':
+        if (s[1] == '+') {
+            cout << "3.3";
+        } else if (s[1] == '0')
+            cout << "3.0";
+        else {
+            cout << "2.7";
+        }
+        break;
+    case 'C':
+        if (s[1] == '+') {
+            cout << "2.3";
+        } else if (s[1] == '0')
+            cout << "2.0";
+        else {
+            cout << "1.7";
+        }
+        break;
+    case 'D':
+        if (s[1] == '+') {
+            cout << "1.3";
+        } else if (s[1] == '0')
+            cout << "1.0";
+        else {
+            cout << "0.7";
+        }
+        break;
+    case 'F':
+        cout << "0.0";
+        break;
+
+    default:
+        break;
+    }
+
+    return 0;
+}
